c882e7e6bb72b6b8e14b5a703bb0d54fcd0c86fc
bolajikassim <kassim.bolaji@yahoo.com>
2025-06-02 00:31:15 +0200

    completed longest_consecutive_sequence.py

---
+++ longest_consecutive_sequence.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def longest_consecutive_sequence(nums: List[int]) -> int:
+    num_set = set(nums)
+    max_count = 0
+
+    for num in nums:
+        if num - 1 not in num_set:
+            current = num + 1
+            count = 1
+
+            while current in num_set:
+                count += 1
+                current += 1
+
+            max_count = max(count, max_count)
+
+    return max_count
+
+print(longest_consecutive_sequence([2,20,4,10,3,4,5]))
+print(longest_consecutive_sequence([0,3,2,5,4,6,1,1]))
